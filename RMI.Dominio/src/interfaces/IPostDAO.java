621c7e0b6d157d284c705b971a295b97283ecfbe
Marcio <marciooizumi@hotmail.com>
2015-12-02 04:21:41 -0200

    alterações nos métodos

--- RMI.Dominio/src/interfaces/IPostDAO.java
+++ RMI.Dominio/src/interfaces/IPostDAO.java
@@ -6,6 +6,6 @@ import java.util.List;
 import model.Post;
 
 public interface IPostDAO extends Remote {
-    public void inserir(Post post) throws RemoteException;
+    public boolean inserir(Post post) throws RemoteException;
     public List<Post> buscarPorUsuario(long usuarioId) throws RemoteException;
 }
--- RMI.Dominio/src/interfaces/IUsuarioDAO.java
+++ RMI.Dominio/src/interfaces/IUsuarioDAO.java
@@ -6,10 +6,11 @@ import java.rmi.RemoteException;
 import java.util.List;
 
 public interface IUsuarioDAO extends Remote{
-    public void inserir(Usuario usuario) throws RemoteException;
+    public boolean inserir(Usuario usuario) throws RemoteException;
     public void atualizar(Usuario usuario) throws RemoteException;
     public Usuario buscarPorId(long id) throws RemoteException;
     public List<Usuario> buscarPorNome(String nome) throws RemoteException;
-    public Usuario buscarPorEmailSenha(String email,String senha) throws RemoteException;
+    public Usuario buscarPorLoginSenha(String login,String senha) throws RemoteException;
     public Usuario buscarPorEmail(String email) throws RemoteException;
+    public List<Usuario> listarTodos()throws RemoteException;
 } 
